128077fb8ead9520c28779e376f65becd0e03f7b
gregbullington <gbullington23@gmail.com>
2022-07-24 15:36:50 -0700

    erc-20-standard-implementation-w-tests

--- test/Token.js
+++ test/Token.js
@@ -1,15 +1,41 @@
 const {expect} = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 describe('Token', () => {
-    //Test code goes here..
-    it('Has a name', async () => {
-        //Fetch Token from blockchain.
+    let token
+
+    beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
-        let token = await Token.deploy()
-        //Reado Token name.
-        const name = await token.name()
-        //Check that name is correct.
-        expect(name).to.equal('My Token')
+        token = await Token.deploy('Dapp Coin', 'DAPP', 1000000)
     })
+
+    describe('Deployment', () => {
+        const name = 'Dapp Coin'
+        const symbol = 'DAPP'
+        const decimal = '18'
+        const totalSupply = tokens("1000000")
+
+
+        it('Has correct name', async () => {
+            expect(await token.name()).to.equal(name)
+        })
+    
+        it('Has correct symbol', async () => {
+            expect(await token.symbol()).to.equal(symbol)
+        })
+    
+        it('Has correct decimal', async () => {
+            expect(await token.decimal()).to.equal(decimal)
+        })
+    
+        it('Has correct total supply', async () => {
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+    })
+    
+
 })
