506ad5402cf90ae07ef312df26c0fc312fb67953
gregbullington <gbullington23@gmail.com>
2022-07-24 23:22:16 -0700

    formatting

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -40,52 +40,51 @@ contract Token {
     function transfer(address _to, uint256 _value)
         public 
         returns (bool success) 
-    {
+        {
 
-        require(balanceOf[msg.sender] >= _value);
+            require(balanceOf[msg.sender] >= _value);
 
-        _transfer(msg.sender, _to, _value);
+            _transfer(msg.sender, _to, _value);
 
-        return true;
-    }
+            return true;
+        }
 
     function _transfer(address _from, address _to, uint256 _value)
-    internal
-    {
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
+        internal
+        {
+            require(_to != address(0));
+            balanceOf[_from] = balanceOf[_from] - _value;
+            balanceOf[_to] = balanceOf[_to] + _value;
 
-        emit Transfer(_from, _to, _value);
+            emit Transfer(_from, _to, _value);
 
-    }
+        }
     
-
     function approve(address _spender, uint256 _value)
         public 
         returns (bool success)
-    {
+        {
 
-        require(_spender != address(0));
+            require(_spender != address(0));
 
 
-        allowance[msg.sender][_spender] = _value;
+            allowance[msg.sender][_spender] = _value;
 
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+            emit Approval(msg.sender, _spender, _value);
+            return true;
+        }
 
     function transferFrom(address _from, address _to, uint256 _value)
         public
         returns(bool success)
-    {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        {
+            require(_value <= balanceOf[_from]);
+            require(_value <= allowance[_from][msg.sender]);
 
-        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+            allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
-        _transfer(_from, _to, _value);
-        return true;
-    }
+            _transfer(_from, _to, _value);
+            return true;
+        }
 
 }
