21fb1a8ca6498553492af5c633ee56ff3ed1489f
gregbullington <gbullington23@gmail.com>
2022-07-24 23:05:59 -0700

    formating

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -76,8 +76,8 @@ contract Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value)
-    public
-    returns(bool success)
+        public
+        returns(bool success)
     {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
