6d3ff30ec9bbda5934632d5251f81cd16318d48b
gregbullington <gbullington23@gmail.com>
2022-07-28 17:33:24 -0700

    removed-bloat-files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <p>This is some placeholder text...</p>
+    </div>
+  );
+}
+
+export default App;
