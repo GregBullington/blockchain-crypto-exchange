543ff8cdb08e0fd09c550026af31d52918298a99
gregbullington <gbullington23@gmail.com>
2022-07-28 20:11:40 -0700

    blockchain-connection-established

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { ethers } from 'ethers';
+import config from '../config.json' 
+import TOKEN_ABI from '../abis/Token.json'
 import '../App.css';
+
 
 function App() {
 
@@ -8,7 +11,16 @@ function App() {
     const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
     console.log(accounts[0])
 
-    
+    //Connect ethers to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const {chainId} = await provider.getNetwork()
+    console.log(chainId)
+
+    //Token smart contract
+    const token = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", TOKEN_ABI, provider)
+    console.log(token.address)
+    const symbol = await token.symbol()
+    console.log(symbol)
   }
 
   useEffect(() => {
