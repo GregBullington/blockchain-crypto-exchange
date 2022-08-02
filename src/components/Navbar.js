52eed4686d3d46d86007f6be0055415a5beb1c01
gregbullington <gbullington23@gmail.com>
2022-08-01 20:38:03 -0700

    navbar-w-account-and-balance

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import logo from '../assets/logo.png'
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    const account = useSelector(state => state.provider.account)
+    const balance = useSelector(state => state.provider.balance)
     return(
       <div className='exchange__header grid'>
         <div className='exchange__header--brand flex'>
-            <img src = {logo} className = "logo" alt = "Xchange logo"></img>
+            <img src = { logo } className = "logo" alt = "Xchange logo"></img>
             <h1> Xchange </h1>
         </div>
   
@@ -13,7 +16,17 @@ const Navbar = () => {
         </div>
   
         <div className='exchange__header--account flex'>
-  
+            { balance ? (
+                <p><small> My Balance: </small> { Number (balance).toFixed(4) } </p>
+            ) : (
+                <p><small> My Balance: </small> 0 ETH </p>
+            )}
+
+            { account ? (
+                <a> {account.slice(0, 5) + "..." + account.slice(38, 42)} </a>
+                ) : (
+                <a></a>
+            )}
         </div>
       </div>
     )
